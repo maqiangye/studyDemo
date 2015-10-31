9b89134b8ddcbd34d0ef1312e7f9fa40f06d3555
maqiangye <maqiangye@163.com>
2015-10-31 22:12:50 +0800

    added test codes

---
+++ commpass/src/main/java/test/MyTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by mqy on 15/10/31.
+ */
+public class MyTest extends InstrumentationTestCase {
+
+    public void test(){
+        final int i=0;
+        final int j=0;
+        assertEquals(i,j);
+    }
+
+}
